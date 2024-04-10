55ffd18e22a0d4448672cc72aec1a070eb358d42
JenniferLS0 <jenniferlimals0@gmail.com>
2024-04-10 16:06:33 -0300

    Tela de Cadastro de usuario

--- Frontend/src/app/Components/feed-component/feed-component.component.ts
+++ Frontend/src/app/Components/feed-component/feed-component.component.ts
@@ -1,12 +1,23 @@
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-feed-component',
   standalone: true,
-  imports: [],
+  imports: [NgbModalModule],
   templateUrl: './feed-component.component.html',
   styleUrl: './feed-component.component.scss'
 })
 export class FeedComponentComponent {
 
+constructor(private modalService: NgbModal) { } // Injete o serviço NgbModal no construtor{
+
+
+   // Método para abrir a modal
+   openModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      // Aqui você pode adicionar qualquer lógica que deseja executar quando a modal é fechada.
+    });
+  }
 }
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuRoutComponentComponent } from './Components/menu-rout-component/menu-rout-component.component';
+import { CadUsuarioComponentComponent } from './Components/cad-usuario-component/cad-usuario-component.component';
+import { FeedComponentComponent } from './Components/feed-component/feed-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuRoutComponentComponent],
+  imports: [RouterOutlet, MenuRoutComponentComponent, CadUsuarioComponentComponent, FeedComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
