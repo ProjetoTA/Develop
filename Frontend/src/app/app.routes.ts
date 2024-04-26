7581cc2ec3f964a9e672e1d189af23ac0f8a446c
JenniferLS0 <jenniferlimals0@gmail.com>
2024-04-26 14:19:46 -0300

    Apagando components da parte de compra da rede

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,9 +3,7 @@ import { CadUsuarioComponentComponent } from './Components/cad-usuario-component
 import { FeedComponentComponent } from './Components/feed-component/feed-component.component';
 import { LoginComponentComponent } from './Components/login-component/login-component.component';
 import { PerfilComponentComponent } from './Components/perfil-component/perfil-component.component';
-import { CadProdutoComponentComponent } from './Components/cad-produto-component/cad-produto-component.component';
 import { CadPetComponentComponent } from './Components/cad-pet-component/cad-pet-component.component';
-import { CadSucessoComponenteComponent } from './Components/cad-sucesso-componente/cad-sucesso-componente.component';
 import { EditPerfilComponentComponent } from './Components/edit-perfil-component/edit-perfil-component.component';
 
 export const routes: Routes = [
@@ -13,8 +11,6 @@ export const routes: Routes = [
      { path: 'feed', component: FeedComponentComponent },
      { path: '', component: LoginComponentComponent },
      { path: 'perfil', component:PerfilComponentComponent  },
-     { path: 'CadProd', component: CadProdutoComponentComponent },
      { path: 'Cadpet', component: CadPetComponentComponent },
-     { path: 'CadSucess', component: CadSucessoComponenteComponent},
      { path: 'editPerfil', component: EditPerfilComponentComponent}
 ];
